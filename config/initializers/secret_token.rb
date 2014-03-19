1ca9cb1fed6dc3e8a6518f8f840526eacb324886
Vladislav Povedyuk <vlad.poveduk@gmail.com>
2014-03-19 10:49:50 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MedicalProject::Application.config.secret_key_base = '251ebe69dc5eb56c4621531b69bab4f3a6d32fd2edf458062f1dec3b5a4d87a0b2907ec570caff5e1f4a068e1e5f64a18bedba7648dc474a97d81b2eed4c8565'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MedicalProject::Application.config.session_store :cookie_store, key: '_MedicalProject_session'
