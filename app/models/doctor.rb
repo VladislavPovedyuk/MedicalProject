7bb5b3ae2e34942474ae035b63a53d36c1a725ef
Alex Shestmintsev <s_a_n_y_o_k90@mail.ru>
2014-04-01 23:49:51 +0300

    Rapair confirmation

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,15 +1,21 @@
 class Doctor < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  :password_confirmation
   devise :database_authenticatable, :validatable
 
 	has_many :meetings
 	has_many :patients, :through => :meetings
 
   validates :name, presence: true, length: { in: 2...15 }
-  validates :login, format: { with: /\A[a-zA-Z0-9]{4;}\z/,
-                              message: 'Only allows letters, numbers. Minimum length for login - 4 characters '}
-  validates :password, format: { with: /\A[a-zA-Z0-9]{6;}\z/,
-                                 message: 'Only allows letters, numbers. Minimum length for password - 6 characters '}
+  #validates :login, format: { with: /\A[a-z0-9_]+\z/,
+  #        message: "%{value} Only allows letters, numbers. Minimum length for login - 4 characters "}
+  validates :email, format: { with: /\A([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}\z/,
+                              message: 'Bad E-mail '}
+  validates :password ,
+  #         confirmation: true
+  #validates :password_confirmation,
+            format: { with: /\A[a-zA-Z0-9_]+\z/,
+            message: 'Only allows letters, numbers. Minimum length for password - 8 characters '}
 
 end
--- app/models/patient.rb
+++ app/models/patient.rb
@@ -5,10 +5,10 @@ class Patient < ActiveRecord::Base
   validates :name, presence: true, length: { in: 2...15 }
   validates :skype, presence: true, length: { in: 2...15 }
 
-  validates :tel, format: { with: /\A[+-0-9]{5;}\z/,
-                            message: 'Only allows numbers and +,-. Minimum length for tel - 5 characters '}
+  validates :tel, format: { with: /\A[0-9+-]+\z/,
+                            message: 'Only allows numbers and +,-. Minimum length for tel - 5 characters '}, length: { in: 5...20 }
   validates :age, numericality: { only_integer: true }
-  validates :gender, inclusion: { in: %w(m f),
+  validates :gender, inclusion: { in: %w(М Ж),
                                   message: "%{value} is not a valid gender" }
 
 end
