e18e6a83ffb261b90d703fdd6b7fcd3dbe9914d9
Alex Shestmintsev <s_a_n_y_o_k90@mail.ru>
2014-04-01 22:38:54 +0300

    Models validation

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -6,4 +6,10 @@ class Doctor < ActiveRecord::Base
 	has_many :meetings
 	has_many :patients, :through => :meetings
 
+  validates :name, presence: true, length: { in: 2...15 }
+  validates :login, format: { with: /\A[a-zA-Z0-9]{4;}\z/,
+                              message: 'Only allows letters, numbers. Minimum length for login - 4 characters '}
+  validates :password, format: { with: /\A[a-zA-Z0-9]{6;}\z/,
+                                 message: 'Only allows letters, numbers. Minimum length for password - 6 characters '}
+
 end
