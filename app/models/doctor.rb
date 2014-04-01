5d0a851b4438e9df383d00b5931bdf146e24fb87
Alex Shestmintsev <s_a_n_y_o_k90@mail.ru>
2014-04-01 23:52:06 +0300

    Fix Doctor Model

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,15 +1,12 @@
 class Doctor < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  :password_confirmation
   devise :database_authenticatable, :validatable
 
 	has_many :meetings
 	has_many :patients, :through => :meetings
 
   validates :name, presence: true, length: { in: 2...15 }
-  #validates :login, format: { with: /\A[a-z0-9_]+\z/,
-  #        message: "%{value} Only allows letters, numbers. Minimum length for login - 4 characters "}
   validates :email, format: { with: /\A([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}\z/,
                               message: 'Bad E-mail '}
   validates :password ,
