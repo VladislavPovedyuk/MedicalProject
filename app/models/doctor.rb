df25a640578c1f2f02b8032d0897df37bf19da24
Alex Shestmintsev <s_a_n_y_o_k90@mail.ru>
2014-04-03 21:15:40 +0300

    Init test

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -14,6 +14,6 @@ class Doctor < ActiveRecord::Base
             format: { with: /\A[a-zA-Z0-9_]+\z/,
             message: 'Only allows letters, numbers. Minimum length for password - 8 characters '}
 
-  scope :sorted_descending, order("created_at DESC")
+  scope :sorted_descending,-> {order("created_at DESC")}
 
 end
