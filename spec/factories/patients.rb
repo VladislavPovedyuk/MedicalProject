2086ce8650dccf4baee8473baf8b2a50e70dfbb8
Alex Shestmintsev <s_a_n_y_o_k90@mail.ru>
2014-04-05 11:24:47 +0300

    spec model validation

--- spec/factories/patients.rb
+++ spec/factories/patients.rb
@@ -1,7 +1,9 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-  factory :patient do
+
+  #valid
+  factory :vasia, class: Patient  do
     name { "Vasya" }
     skype { "vasia" }
     tel {"06611111111"}
@@ -9,5 +11,38 @@ FactoryGirl.define do
     gender {"М"}
   end
 
+  #not valid
+  factory :vasia_error1, class: Patient  do
+    name { "" }
+    skype { "" }
+    tel {""}
+    age {}
+    gender {""}
+  end
+
+  factory :vasia_error2, class: Patient  do
+    name { "Vasya" }
+    skype { "vasia" }
+    tel {""}
+    age {18}
+    gender {"М"}
+  end
+
+  factory :vasia_error3, class: Patient  do
+    name { "Vasya" }
+    skype { "vasia" }
+    tel {"123"}
+    age {1}
+    gender {"m"}
+  end
+
+  factory :vasia_error4, class: Patient  do
+    name { "Vasya" }
+    skype { "vasia" }
+  end
+
+  factory :vasia_empty, class: Patient  do
+
+  end
 
 end
--- spec/models/patient_spec.rb
+++ spec/models/patient_spec.rb
@@ -2,8 +2,30 @@ require 'spec_helper'
 
 describe Patient do
 
+  #valid
   it "has a valid factory" do
-    FactoryGirl.build(:patient).should be_valid
+    FactoryGirl.build(:vasia).should be_valid
+  end
+
+  #not valid
+  it "has a not valid factory" do
+    FactoryGirl.build(:vasia_error1).should_not be_valid
+  end
+
+  it "has a not valid factory" do
+    FactoryGirl.build(:vasia_error2).should_not be_valid
+  end
+
+  it "has a not valid factory" do
+    FactoryGirl.build(:vasia_error3).should_not be_valid
+  end
+
+  it "has a not valid factory" do
+    FactoryGirl.build(:vasia_error4).should_not be_valid
+  end
+
+  it "has a not valid factory" do
+    FactoryGirl.build(:vasia_empty).should_not be_valid
   end
 
   it { should have_many(:meetings).with_foreign_key('patient_id') }
